ba0bf2cef46951761f5731b4a9d27dc15c3a6c22
J Lee <jay.j.lee.1@gmail.com>
2021-12-20 20:22:51 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+init()
+
+function init() {
+  handleForm()
+  handleClicks()
+}
+
+function handleForm(){
+  //grab the form  
+  const form = document.getElementById("create-task-form");
+  // add 'submit' event listener to the form
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderTask(event);
+    event.target.reset()
+
+
+  })
+
+}
+
+function renderTask(event) {
+  const input = event.target["new-task-description"].value
+  const li = document.createElement("li")
+  li.innerText = input
+  const delButton = document.createElement("button")
+  delButton.innerText = "x"
+  li.append(delButton)
+  delButton.addEventListener("click", () => {
+    li.remove()
+  })
+  document.getElementById('tasks').appendChild(li)
+  
+}
+
+// function handleClicks() {
+//   document.getElementById('tasks').addEventListener('click', (e) => {
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+//     // if the e.target is an li remove it, else do nothing
+//     if (e.target.nodeName == "BUTTON") {
+//       e.target.parentNode.remove()
+//     } else {
+//       console.log("cant do that");
+//     }
+//   })
+// }
